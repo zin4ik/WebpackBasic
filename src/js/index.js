ba21544905f6d3a31af2a2722a517ce038f26f6a
Serey <zin4ik86@gmail.com>
2019-08-22 00:27:03 +0300

    add browserslist

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,23 @@ import '../scss/main.scss'
 import '../bootstrap-castum/castum-bootstrap.js'
 
 console.log('yes');
+
+
 jQuery(document).ready(function($){
     $('h1').css('color','red');
 })
+
+let options = {
+	id: 'elem',
+	color: 'blue',
+	border: '1px solid red',
+	font: '15px Arial'
+};
+
+function func({id, color = 'blue', border='1px dotted red', font='15px Tahoma'}) {
+	let e = document.getElementById(id);
+	let str = 'color:'+color+'; border:'+border+'; font:'+font;
+	e.style.cssText = str;
+}
+
+func(options);
