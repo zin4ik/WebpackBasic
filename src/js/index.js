589c3f866d7d86b3e7a7c6c32c185cbf7e6dd645
Serey <zin4ik86@gmail.com>
2019-08-21 17:24:29 +0300

    webpack (scss+bootstrap)

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import '../scss/main.scss'
+
+
+console.log('yes');
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+
+
+/*=======вхідні і вихідні каталоги========*/
+const PATHS ={
+src: path.join(__dirname, './src'),
+dist: path.join(__dirname, './dist')
+};
+
+/*=====модулі точок(файлів)входа і продакшин====*/
+module.exports = {
+     mode: 'development',
+//  mode:'production',
+    devServer: {
+        port: 8081,
+        overlay: {
+            warnings: false,
+            errors: true
+        }
+    },
+    externals: {
+        paths: PATHS
+      },
+
+  entry: `${PATHS.src}/js/index.js`,
+  devtool: 'inline-source-map',
+  output: {
+    path: PATHS.dist,
+    filename: 'js/[name].[hash].js',
+    publicPath: '/'
+  },
+
+   /*====loaders===*/
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [  
+            'style-loader',
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true }
+            }, {
+              loader: 'postcss-loader',
+              options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+            }, {
+              loader: 'sass-loader',
+              options: { sourceMap: true }
+            }
+          ]
+        }
+    ],
+  },
+
+
+/*====plugins====*/
+plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template:`${PATHS.src}/index.html`
+        //   title: 'Output Management'
+        }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: 'css/[name].[hash].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+
+  ]
+
+}
+          
