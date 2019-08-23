c21763f7d4c2e25175197bce6a5a2a0c30758b94
Serey <zin4ik86@gmail.com>
2019-08-23 12:34:29 +0300

    remove config.prod

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,10 +1,14 @@
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.config')
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const buildWebpackConfig = merge(baseWebpackConfig, {
   // BUILD config
   mode: 'production',
-  plugins: []
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
 })
 
 module.exports = new Promise((resolve, reject) => {
